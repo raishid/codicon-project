e761b511893b25843d824a715377d639faffa20f
D0ble2 <angelcalderon303@gmail.com>
2023-03-11 02:13:55 -0400

    landing page iniciada y entorno configurado

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './scss/styles.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import "./style/flexboxgrid.min.css";
+import './style/index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <>
     <App />
-  </React.StrictMode>,
-)
+  </>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
