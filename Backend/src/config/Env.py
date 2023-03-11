6fdcd4983b94da0ef6be0827faf5b96719401660
GabrielEHT <gabrielhernandez202333@gmail.com>
2023-03-11 14:35:21 -0400

    Change ambiguos exception raise
    Se ha cambiado la excepción ambigua que se lanzaba al no encontrarse el
    archivo .env por la excepción FileNotFoundError

--- Backend/src/config/Env.py
+++ Backend/src/config/Env.py
@@ -4,6 +4,6 @@ import os
 def env(key: str):
     pathEnv = os.path.join(os.path.dirname(__file__), "../../", ".env")
     if not os.path.exists(pathEnv):
-        raise Exception("Archivo .env no encontrado")
+        raise FileNotFoundError("Archivo .env no encontrado")
     
     return dotenv_values(pathEnv)[key] if key in dotenv_values(pathEnv) else None
