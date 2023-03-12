3e067e77e126d27318f7a2a3c464af9e6ffd6788
fededav <67575679+raishid@users.noreply.github.com>
2023-03-12 13:29:45 -0400

    Update perritos.js
    agregando raza

--- Frontend/src/contants/perritos.js
+++ Frontend/src/contants/perritos.js
@@ -1,72 +1,96 @@
 const perros = [
     {
-      nombre: "Firulais",
-      sexo: "Macho",
-      tamaño: "Pequeño",
-      cualidades: ["Juguetón", "Leal", "Inteligente"],
-      ciudad: "Caracas"
+        nombre: "Firulais",
+        sexo: "Macho",
+        tamaño: "Pequeño",
+        cualidades: [
+            "Juguetón", "Leal", "Inteligente"
+        ],
+        ciudad: "Caracas",
+        raza: "Mestizo"
     },
     {
-      nombre: "Luna",
-      sexo: "Hembra",
-      tamaño: "Mediano",
-      cualidades: ["Amigable", "Protectiva", "Cariñosa"],
-      ciudad: "Maracaibo"
+        nombre: "Luna",
+        sexo: "Hembra",
+        tamaño: "Mediano",
+        cualidades: [
+            "Amigable", "Protectiva", "Cariñosa"
+        ],
+        ciudad: "Maracaibo",
+        raza: "Mestizo"
     },
     {
-      nombre: "Thor",
-      sexo: "Macho",
-      tamaño: "Grande",
-      cualidades: ["Fuerte", "Valiente", "Obediente"],
-      ciudad: "Valencia"
+        nombre: "Thor",
+        sexo: "Macho",
+        tamaño: "Grande",
+        cualidades: [
+            "Fuerte", "Valiente", "Obediente"
+        ],
+        ciudad: "Valencia",
+        raza: "Mestizo"
     },
     {
-      nombre: "Molly",
-      sexo: "Hembra",
-      tamaño: "Pequeño",
-      cualidades: ["Divertida", "Inquieta", "Curiosa"],
-      ciudad: "Barquisimeto"
-    },
-    {
-      nombre: "Rocky",
-      sexo: "Macho",
-      tamaño: "Grande",
-      cualidades: ["Energético", "Afectuoso", "Entrenable"],
-      ciudad: "Caracas"
-    },
-    {
-      nombre: "Kiara",
-      sexo: "Hembra",
-      tamaño: "Mediano",
-      cualidades: ["Tranquila", "Leal", "Cuidadosa"],
-      ciudad: "Maracaibo"
-    },
-    {
-      nombre: "Max",
-      sexo: "Macho",
-      tamaño: "Pequeño",
-      cualidades: ["Adorable", "Amigable", "Divertido"],
-      ciudad: "Valencia"
-    },
-    {
-      nombre: "Bella",
-      sexo: "Hembra",
-      tamaño: "Mediano",
-      cualidades: ["Inteligente", "Leal", "Protectiva"],
-      ciudad: "Barquisimeto"
-    },
-    {
-      nombre: "Bruno",
-      sexo: "Macho",
-      tamaño: "Grande",
-      cualidades: ["Fuerte", "Leal", "Obediente"],
-      ciudad: "Caracas"
-    },
-    {
-      nombre: "Daisy",
-      sexo: "Hembra",
-      tamaño: "Pequeño",
-      cualidades: ["Cariñosa", "Divertida", "Curiosa"],
-      ciudad: "Maracaibo"
+        nombre: "Molly",
+        sexo: "Hembra",
+        tamaño: "Pequeño",
+        cualidades: [
+            "Divertida", "Inquieta", "Curiosa"
+        ],
+        ciudad: "Barquisimeto",
+        raza: "Mestizo"
+    }, {
+        nombre: "Rocky",
+        sexo: "Macho",
+        tamaño: "Grande",
+        cualidades: [
+            "Energético", "Afectuoso", "Entrenable"
+        ],
+        ciudad: "Caracas",
+        raza: "Mestizo"
+    }, {
+        nombre: "Kiara",
+        sexo: "Hembra",
+        tamaño: "Mediano",
+        cualidades: [
+            "Tranquila", "Leal", "Cuidadosa"
+        ],
+        ciudad: "Maracaibo",
+        raza: "Mestizo"
+    }, {
+        nombre: "Max",
+        sexo: "Macho",
+        tamaño: "Pequeño",
+        cualidades: [
+            "Adorable", "Amigable", "Divertido"
+        ],
+        ciudad: "Valencia",
+        raza: "Mestizo"
+    }, {
+        nombre: "Bella",
+        sexo: "Hembra",
+        tamaño: "Mediano",
+        cualidades: [
+            "Inteligente", "Leal", "Protectiva"
+        ],
+        ciudad: "Barquisimeto",
+        raza: "Mestizo"
+    }, {
+        nombre: "Bruno",
+        sexo: "Macho",
+        tamaño: "Grande",
+        cualidades: [
+            "Fuerte", "Leal", "Obediente"
+        ],
+        ciudad: "Caracas",
+        raza: "Mestizo"
+    }, {
+        nombre: "Daisy",
+        sexo: "Hembra",
+        tamaño: "Pequeño",
+        cualidades: [
+            "Cariñosa", "Divertida", "Curiosa"
+        ],
+        ciudad: "Maracaibo",
+        raza: "Mestizo"
     }
-  ];
+];
