44438754eed03b4fe84fe13ee89d9b178cddef96
GabrielEHT <gabrielhernandez202333@gmail.com>
2023-03-12 10:24:47 -0400

    Change mascota/random endpoint to update db entry
    El endpoint mascota/random ahora actualiza el estado de adopción de la
    mascota de "libre" a "en proceso"

--- Backend/src/controllers/AdopcionController.py
+++ Backend/src/controllers/AdopcionController.py
@@ -1,5 +1,6 @@
 from random import choice
 from flask import Blueprint
+from src.config.Database import db
 from src.models.Mascota import Mascota
 
 url_prefix = '/v1'
@@ -10,5 +11,8 @@ def random_adoption():
     mascotas_disponibles = Mascota.query.filter_by(estado=1).all()
 
     mascota = choice(mascotas_disponibles)
+    mascota.estado = 2
+
+    db.session.commit()
 
     return mascota.serialize()
